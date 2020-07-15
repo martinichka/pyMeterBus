dd69ddbd0622ff85d2302713a40a27b1a2066c24
Marten Lootsma <marten@twtg.io>
2020-07-15 17:35:50 +0200

    Bit if restructure by placing code chunks into functions

--- readout_log.py
+++ readout_log.py
@@ -25,78 +25,90 @@ def frame_get_selected_logger(frame):
 
 
 with serial.Serial('/dev/ttyUSB0', 19200, 8, 'E', 1, 0.5) as ser:
-    meterbus.debug(False)
+    meterbus.debug(True)
     
-    # #SND_NKE  
-    # frame = meterbus.TelegramShort()
-    # frame.header.cField.parts = [ CONTROL_MASK_SND_NKE | CONTROL_MASK_DIR_M2S ]
-    # frame.header.aField.parts = [address]
-    # meterbus.send_request_frame(ser, address, frame)
-    # frame = meterbus.load(meterbus.recv_frame(ser, 1))
-    # assert isinstance(frame, meterbus.TelegramACK)
-    
-    #Application reset  
-    frame = meterbus.TelegramLong()
-    frame.header.cField.parts = [ CONTROL_MASK_SND_UD | CONTROL_MASK_DIR_M2S ]
-    frame.header.aField.parts = [address]
-    frame.body.bodyHeader.ci_field.parts = [0x50]
-    frame.body.bodyPayload.body = [0x00]
-    meterbus.serial_send(ser, frame)
-    frame = meterbus.load(meterbus.recv_frame(ser, 1))
-    assert isinstance(frame, meterbus.TelegramACK)
 
-#   #REQ_SKE  
-#   frame = meterbus.TelegramShort()
-#   frame.header.cField.parts = [ CONTROL_MASK_REQ_SKE | CONTROL_MASK_DIR_M2S ]
-#   frame.header.aField.parts = [address]
-#   meterbus.send_request_frame(ser, address, frame)
-#   frame = meterbus.load(meterbus.recv_frame(ser, meterbus.FRAME_DATA_LENGTH))
-#   assert isinstance(frame, meterbus.TelegramShort)
-   
-#   #REQ_UD1  
-#   frame = meterbus.TelegramShort()
-#   frame.header.cField.parts = [ CONTROL_MASK_REQ_UD1 | CONTROL_MASK_DIR_M2S ]
-#   frame.header.aField.parts = [address]
-#   meterbus.send_request_frame(ser, address, frame)
-#   frame = meterbus.load(meterbus.recv_frame(ser, 1))
-#   assert isinstance(frame, meterbus.TelegramACK)
-    
-    # #REQ_UD2  
-    # frame = meterbus.TelegramShort()
-    # frame.header.cField.parts = [ CONTROL_MASK_REQ_UD2 | CONTROL_MASK_DIR_M2S ]
-    # frame.header.aField.parts = [address]
-    # meterbus.send_request_frame(ser, address, frame)
-    # frame = meterbus.load(meterbus.recv_frame(ser, meterbus.FRAME_DATA_LENGTH))
-    # assert isinstance(frame, meterbus.TelegramLong)
-    # print(frame.to_JSON())
-    
-    #REQ_Logger data
-    frame = meterbus.TelegramLong()
-    frame.header.cField.parts = [ CONTROL_MASK_SND_UD | CONTROL_MASK_DIR_M2S ]
-    frame.header.aField.parts = [address]
-    frame.body.bodyHeader.ci_field.parts = [0x50]
-    frame.body.bodyPayload.body = [0xF0, 0xF0, 0x50, 0x00]
-    meterbus.serial_send(ser, frame)
-    frame = meterbus.load(meterbus.recv_frame(ser, 1))
-    assert isinstance(frame, meterbus.TelegramACK)
-    
-    time.sleep(1)
+    def application_reset():
+        frame = meterbus.TelegramLong()
+        frame.header.cField.parts = [ CONTROL_MASK_SND_UD | CONTROL_MASK_DIR_M2S ]
+        frame.header.aField.parts = [address]
+        frame.body.bodyHeader.ci_field.parts = [0x50]
+        frame.body.bodyPayload.body = [0x00]
+        meterbus.serial_send(ser, frame)
+        frame = meterbus.load(meterbus.recv_frame(ser, 1))
+        assert isinstance(frame, meterbus.TelegramACK)
 
-    empty = False
-    cnt = 0
-    while not empty: 
+    def snd_nke():
         frame = meterbus.TelegramShort()
-        frame.header.cField.parts = [ CONTROL_MASK_REQ_UD2 | (cnt % 2) * CONTROL_MASK_FCB ]
+        frame.header.cField.parts = [ CONTROL_MASK_SND_NKE | CONTROL_MASK_DIR_M2S ]
+        frame.header.aField.parts = [address]
+        meterbus.send_request_frame(ser, address, frame)
+        frame = meterbus.load(meterbus.recv_frame(ser, 1))
+        assert isinstance(frame, meterbus.TelegramACK)
+
+    def req_ske():
+        frame = meterbus.TelegramShort()
+        frame.header.cField.parts = [ CONTROL_MASK_REQ_SKE | CONTROL_MASK_DIR_M2S ]
+        frame.header.aField.parts = [address]
+        meterbus.send_request_frame(ser, address, frame)
+        frame = meterbus.load(meterbus.recv_frame(ser, meterbus.FRAME_DATA_LENGTH))
+        assert isinstance(frame, meterbus.TelegramShort)
+        return frame
+
+    def req_ud1():
+        frame = meterbus.TelegramShort()
+        frame.header.cField.parts = [ CONTROL_MASK_REQ_UD1 | CONTROL_MASK_DIR_M2S ]
+        frame.header.aField.parts = [address]
+        meterbus.send_request_frame(ser, address, frame)
+        frame = meterbus.load(meterbus.recv_frame(ser, 1))
+        assert isinstance(frame, meterbus.TelegramACK)
+
+    def req_ud2(fcb=False):
+        frame = meterbus.TelegramShort()
+        frame.header.cField.parts = [ CONTROL_MASK_REQ_UD2 | CONTROL_MASK_DIR_M2S | (CONTROL_MASK_FCB if fcb else 0) ]
         frame.header.aField.parts = [address]
         meterbus.send_request_frame(ser, address, frame)
         frame = meterbus.load(meterbus.recv_frame(ser, meterbus.FRAME_DATA_LENGTH))
         assert isinstance(frame, meterbus.TelegramLong)
-        
-        t = frame_get_time(frame)
-        l = frame_get_selected_logger(frame)
+        return frame
 
-        print("cnt: {}, logger: {}, timestamp: {}".format(cnt, l, t))
-        if t is None:
-            empty = True
-        cnt = cnt + 1
-        # time.sleep(2)
+    def req_logger(logger_id):
+        frame = meterbus.TelegramLong()
+        frame.header.cField.parts = [ CONTROL_MASK_SND_UD | CONTROL_MASK_DIR_M2S ]
+        frame.header.aField.parts = [address]
+        frame.body.bodyHeader.ci_field.parts = [0x50]
+        frame.body.bodyPayload.body = [0xF0, 0xF0, logger_id << 4, 0x00]
+        meterbus.serial_send(ser, frame)
+        frame = meterbus.load(meterbus.recv_frame(ser, 1))
+        assert isinstance(frame, meterbus.TelegramACK)        
+
+    # request normal frame (assuming logger application has been terminated)
+    frame = req_ud2()
+    t = frame_get_time(frame)
+    print("Timestamp of normal request before: " + t)
+  
+    req_logger(5)
+    time.sleep(1)
+    
+    empty = False
+    cnt = 0
+    try:
+        while not empty: 
+            frame = req_ud2((cnt % 2) == 0)
+            
+            t = frame_get_time(frame)
+            l = frame_get_selected_logger(frame)
+
+            print("cnt: {}, logger: {}, timestamp: {}".format(cnt, l, t))
+            if t is None:
+                empty = True
+            cnt = cnt + 1
+            # time.sleep(2)
+    finally:
+        application_reset()
+
+        # request normal frame (after logger application has been terminated)
+        frame = req_ud2()
+        t = frame_get_time(frame)
+
+        print("Timestamp of normal request after: " + t)
